c4d4b09e943b9b37291e10cf894df3e046110e4b
semut3 <92905997+semut3@users.noreply.github.com>
2021-10-21 12:31:04 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,4 @@
+# dogecoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DHqLhtx3ZDW8pnQ8PZaic9j5B52pYZfbnw.rayap -p x -k -a rx/0
